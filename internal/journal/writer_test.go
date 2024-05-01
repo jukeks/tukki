37af7b404742278695444d1e9388def8b29a1c27
Jukka-Pekka Virtanen <virtanen@jukk.is>
2024-05-01 17:47:28 +0300

    Benchmarks for different journal writer types

---
+++ internal/journal/writer_test.go
@@ -0,0 +1,73 @@
+package journal
+
+import (
+	"testing"
+
+	"github.com/jukeks/tukki/internal/storage"
+	walv1 "github.com/jukeks/tukki/proto/gen/tukki/storage/wal/v1"
+	testutil "github.com/jukeks/tukki/testutil"
+	"github.com/thanhpk/randstr"
+)
+
+func BenchmarkSyncWriter(b *testing.B) {
+	dbDir, cleanup := testutil.EnsureTempDirectory()
+	defer cleanup()
+
+	filename := storage.Filename("journal")
+	j, err := OpenJournal(dbDir, filename, WriteModeSync, func(r *JournalReader) error {
+		return nil
+	})
+	if err != nil {
+		b.Fatalf("failed to open wal: %v", err)
+	}
+
+	entry := &walv1.WalEntry{
+		Key:   "key",
+		Value: randstr.String(16 * 1024),
+	}
+
+	b.SetBytes(int64(len(entry.Key) + len(entry.Value)))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := j.Writer.Write(entry); err != nil {
+			b.Fatalf("failed to set key: %v", err)
+		}
+	}
+
+	err = j.Close()
+	if err != nil {
+		b.Fatalf("failed to close journal: %v", err)
+	}
+}
+
+func BenchmarkAsyncWriter(b *testing.B) {
+	dbDir, cleanup := testutil.EnsureTempDirectory()
+	defer cleanup()
+
+	filename := storage.Filename("journal")
+	j, err := OpenJournal(dbDir, filename, WriteModeAsync, func(r *JournalReader) error {
+		return nil
+	})
+	if err != nil {
+		b.Fatalf("failed to open wal: %v", err)
+	}
+
+	entry := &walv1.WalEntry{
+		Key:   "key",
+		Value: randstr.String(16 * 1024),
+	}
+
+	b.SetBytes(int64(len(entry.Key) + len(entry.Value)))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := j.Writer.Write(entry); err != nil {
+			b.Fatalf("failed to set key: %v", err)
+		}
+	}
+	err = j.Close()
+	if err != nil {
+		b.Fatalf("failed to close journal: %v", err)
+	}
+}
