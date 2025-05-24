5165f7f8a720cc822f866c482ba55c31e52e7627
Jukka-Pekka Virtanen <virtanen@jukk.is>
2025-05-24 17:37:44 +0300

    Expose types

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,10 +2,13 @@ package db
 
 import "github.com/jukeks/tukki/internal/db"
 
-func GetDefaultConfig() db.Config {
+type Database = db.Database
+type Config = db.Config
+
+func GetDefaultConfig() Config {
 	return db.GetDefaultConfig()
 }
 
-func OpenDatabase(dbDir string, config db.Config) (*db.Database, error) {
+func OpenDatabase(dbDir string, config Config) (*Database, error) {
 	return db.OpenDatabaseWithConfig(dbDir, config)
 }
